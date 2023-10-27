510144913c8f3e24d6e8cf3eb13835a215e797fd
jorgeguedess <jorge_guedes00@hotmail.com>
2023-10-27 16:19:05 -0300

    feat: add Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,53 @@
-const Card = () => {
-  return <div>Card</div>;
+import Image from "next/image";
+import { ICar } from "../../types/car";
+import { Block, Flex, Link, Text } from "vcc-ui";
+
+import styles from "./Card.module.css";
+
+const Card = ({ bodyType, modelName, modelType, imageUrl }: ICar) => {
+  return (
+    <Block>
+      <Link href="#" className={styles.card}>
+        <Text
+          extend={{
+            textTransform: "uppercase",
+            fontWeight: "500 !important",
+          }}
+          variant="bates"
+          foreground="foreground.secondary"
+        >
+          {bodyType}
+        </Text>
+        <Text
+          extend={{
+            fontWeight: "bold !important",
+            marginBottom: "1.25rem",
+          }}
+        >
+          {modelName} <span className={styles.modelType}>{modelType}</span>
+        </Text>
+
+        <Image src={imageUrl} alt={modelName} width={800} height={600} />
+      </Link>
+
+      <Flex
+        extend={{
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "2rem",
+        }}
+      >
+        <Link href="#" arrow="right">
+          Learn
+        </Link>
+
+        <Link href="#" arrow="right">
+          Shop
+        </Link>
+      </Flex>
+    </Block>
+  );
 };
 
 export default Card;
